f3cdee343259f3cea9c4dde74f5196b55365acea
Artem Hmel <artemhmel6@gmail.com>
2021-06-29 08:51:02 +1000

    add page component for children routes

---
+++ src/components/layout/Page.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="kmtt-page">
+    666
+    {{ title }}
+    {{ nameLink }}
+    {{ title123 }}
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+const MenuBar = () => import('../core/Menu.vue');
+
+@Component({
+  components: {
+    MenuBar
+  }
+})
+export default class Page extends Vue {
+  @Prop() title: String | undefined;
+  @Prop() nameLink: String | undefined;
+  @Prop() title123: String | undefined;
+}
+</script>
+
+<style>
+/* .kmtt-page-layout {
+  display: flex;
+  width: 100%;
+  height: 100%;
+} */
+</style>
