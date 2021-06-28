c3075753331f9ac435ae736ec4a268be1851eb0c
Artem Hmel <artemhmel6@gmail.com>
2021-06-29 09:46:16 +1000

    add button

---
+++ src/components/core/Button.vue
@@ -0,0 +1,23 @@
+<template>
+  <button v-on="$listeners" @click="action" class="kmtt-button">{{ text }}</button>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+@Component
+export default class Layout extends Vue {
+  @Prop() text: String | undefined;
+  @Prop() action: void | undefined;
+}
+</script>
+
+<style>
+.kmtt-button {
+  color: #fff;
+  padding: 12px 16px;
+  border-radius: 12px;
+  background-color: #3680ec;
+  cursor: pointer;
+}
+</style>
