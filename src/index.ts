531d1a58bd9b0f54439907b2e4f2fdbca5427ee0
Artem Hmel <artemhmel6@gmail.com>
2021-06-29 08:51:28 +1000

    add func getRoutes for config

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,34 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import App from './components/App.vue';
-import Layout from './components/layout/Layout.vue';
 
-import routes from './config.pages';
+import routesConfig from './config.pages';
 
 require('./assets/style.css');
 
 Vue.use(VueRouter);
 
+const Layout = () => import('./components/layout/Layout.vue');
+const Page = () => import('./components/layout/Page.vue');
+
+const getRoutes = (config: any) => {
+  return config.map((el: any) => {
+    console.log(el.page);
+    const route = {
+      path: el.path,
+      component: el.page ? Layout : Page,
+      props: el.props,
+      children: ''
+    }
+    if (el.children) route.children = getRoutes(el.children);
+    return route;
+  });
+}
+
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
-  routes:
-  routes.pages.map((el: any) => {
-    return {
-      path: el.path,
-      component: Layout,
-      props: {...routes.propsLayout, ...el.props}
-    }
-  })
+  routes: getRoutes(routesConfig),
 })
 
 new Vue({
