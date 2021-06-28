660ea3aa841ffc3aea2a764d57471b7ef3ba8d1e
Artem Hmel <artemhmel6@gmail.com>
2021-06-28 16:24:50 +1000

    add layout

---
+++ src/components/layout/Layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="kmtt-page-layout">
+    <MenuBar v-if="menu" :menu="menu" :title="title" />
+    <div></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+
+const MenuBar = () => import('../core/Menu.vue');
+
+@Component({
+  components: {
+    MenuBar
+  }
+})
+export default class Layout extends Vue {
+  @Prop() title: String | undefined;
+  @Prop() menu: string | undefined;
+}
+</script>
+
+<style>
+.kmtt-page-layout {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+</style>
