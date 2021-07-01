69cc774562a26a26d2fcd699657dcaa2cf75f90e
Artem Hmel <artemhmel6@gmail.com>
2021-07-01 12:19:32 +1000

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   output: {
     filename: '[name].bundle.js',
     chunkFilename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   module: {
     rules: [
